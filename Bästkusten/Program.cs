8f5ead375eff2a4bfb08e28ecb7bb79d72770530
Elin <89elibju@gafe.molndal.se>
2020-12-09 13:07:04 +0100

    Add project files.

---
+++ Bästkusten/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Bästkusten
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Student
+    {
+        private string name;
+        private int age;
+        private double height;
+        private string hometown;
+        private string birthtown;
+        private string hobby;
+        private string favFood;
+        private string favColor;
+        private int nrOfSiblings;
+        private string motivation;
+
+        public Student()
+        {
+
+        }
+
+
+        public Student(string name, int age, double height, string hometown, string birthtown, string hobby, string favFood, string favColor, int nrOfSiblings, string motivation)
+        {
+
+        }
+
+       
+
+    }
+}
